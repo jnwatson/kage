b4cd96fb45499b2c88f58d4954ae23f5045fd025
Nic Watson <nicwatson@google.com>
2026-01-21 02:02:54 +0000

    Refactor altinstructions handling

--- arch/arm64/kernel/module.c
+++ arch/arm64/kernel/module.c
@@ -31,6 +31,9 @@
 static u64 module_direct_base __ro_after_init = 0;
 static u64 module_plt_base __ro_after_init = 0;
 
+// Nic tmp
+#pragma clang optimize off 
+
 /*
  * Choose a random page-aligned base address for a window of 'size' bytes which
  * entirely contains the interval [start, end - 1].
